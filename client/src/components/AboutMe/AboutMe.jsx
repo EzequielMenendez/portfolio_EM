66ba84c4ba504ed04fd718ccda1aaeb8844651bc
EzequielMenendez <ezemenendez2003@gmail.com>
2023-11-17 11:24:16 -0300

    add css

--- client/src/components/AboutMe/AboutMe.jsx
+++ client/src/components/AboutMe/AboutMe.jsx
@@ -1,19 +1,19 @@
-import { DivAbout } from "./StyledAboutMe"
+import { DivAbout, DivText, H2About, H3About, PAbout } from "./StyledAboutMe"
 import { Slide } from "react-reveal"
 
 const AboutMe = ()=>{
     return (
         <Slide left>
             <DivAbout id="about">
-                <h2>Sobre Mí</h2>
-                <div>
-                    <p>Hola 👋, soy Ezequiel Menéndez, un Desarrollador Web Full-Stack. Tengo 20 años y desde una edad temprana me interesó el mundo de la tecnología, lo cual me llevo al mundo de la programación.</p>
-                </div>
-                <h3>Objetivos</h3>
-                <div>
-                    <p>Mi objetivo es generar un impacto y contribuir significativamente al campo de la programación.</p>
-                    <p>Lo que más me apasiona de la programación y lo que me motiva a seguir adelante es la búsqueda de soluciones a los desafíos que se nos presentan, siempre explorando múltiples perspectivas.</p>
-                </div>
+                <H2About>Sobre Mí</H2About>
+                <DivText>
+                    <PAbout>Hola 👋, soy Ezequiel Menéndez, un Desarrollador Web Full-Stack. Tengo 20 años y desde una edad temprana me interesó el mundo de la tecnología, lo cual me llevo al mundo de la programación.</PAbout>
+                </DivText>
+                <H3About>Objetivos</H3About>
+                <DivText>
+                    <PAbout>Mi objetivo es generar un impacto y contribuir significativamente al campo de la programación.</PAbout>
+                    <PAbout>Lo que más me apasiona de la programación y lo que me motiva a seguir adelante es la búsqueda de soluciones a los desafíos que se nos presentan, siempre explorando múltiples perspectivas.</PAbout>
+                </DivText>
             </DivAbout>
         </Slide>
     )
--- client/src/components/AboutMe/StyledAboutMe.js
+++ client/src/components/AboutMe/StyledAboutMe.js
@@ -8,3 +8,25 @@ export const DivAbout = styled.div`
     background-color: #ffffff;
     height: 70vh;
 `
+
+export const H2About = styled.h2`
+    background-color: #ffffff;
+    font-size: 27px;
+    margin: 12px;
+`
+
+export const H3About = styled.h3`
+    background-color: #ffffff;
+    font-size: 23px;
+    margin: 12px;
+`
+
+export const PAbout = styled.p`
+    background-color: #ffffff;
+    padding: 7px;
+    font-size: 16.5px;
+`
+
+export const DivText = styled.div`
+    max-width: 65vw;
+`
