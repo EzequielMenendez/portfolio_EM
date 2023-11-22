d57b2def53afd9ebb76170fe920710437d32777b
EzequielMenendez <ezemenendez2003@gmail.com>
2023-11-22 13:09:13 -0300

    update photos

--- client/src/components/AboutMe/AboutMe.jsx
+++ client/src/components/AboutMe/AboutMe.jsx
@@ -1,19 +1,25 @@
-import { DivAbout, DivText, H2About, H3About, PAbout } from "./StyledAboutMe"
+import { DivAbout, DivText, H2About, H3About, PAbout, DivInfo, ImgAbout, DivImg } from "./StyledAboutMe"
+import FotoEze from "../../utils/FotoEzePerfil.jpg"
 import { Slide } from "react-reveal"
 
 const AboutMe = ()=>{
     return (
         <Slide bottom>
             <DivAbout id="about">
-                <H2About>Sobre Mí</H2About>
-                <DivText>
-                    <PAbout>Hola 👋, soy Ezequiel Menéndez, un Desarrollador Web Full-Stack. Tengo 20 años y desde una edad temprana me interesó el mundo de la tecnología, lo cual me llevo al mundo de la programación.</PAbout>
-                </DivText>
-                <H3About>Objetivos</H3About>
-                <DivText>
-                    <PAbout>Mi objetivo es generar un impacto y contribuir significativamente al campo de la programación.</PAbout>
-                    <PAbout>Lo que más me apasiona de la programación y lo que me motiva a seguir adelante es la búsqueda de soluciones a los desafíos que se nos presentan, siempre explorando múltiples perspectivas.</PAbout>
-                </DivText>
+                <DivInfo>
+                    <H2About>Sobre Mí</H2About>
+                    <DivText>
+                        <PAbout>Hola 👋, soy Ezequiel Menéndez, un Desarrollador Web Full-Stack. Tengo 20 años y desde una edad temprana me interesó el mundo de la tecnología, lo cual me llevo al mundo de la programación.</PAbout>
+                    </DivText>
+                    <H3About>Objetivos</H3About>
+                    <DivText>
+                        <PAbout>Mi objetivo es generar un impacto y contribuir significativamente al campo de la programación.</PAbout>
+                        <PAbout>Lo que más me apasiona de la programación y lo que me motiva a seguir adelante es la búsqueda de soluciones a los desafíos que se nos presentan, siempre explorando múltiples perspectivas.</PAbout>
+                    </DivText>
+                </DivInfo>
+                <DivImg>
+                    <ImgAbout src={FotoEze}/>
+                </DivImg>
             </DivAbout>
         </Slide>
     )
--- client/src/components/AboutMe/StyledAboutMe.js
+++ client/src/components/AboutMe/StyledAboutMe.js
@@ -2,11 +2,21 @@ import styled from 'styled-components'
 
 export const DivAbout = styled.div`
     display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+    background-color: #ffffff;
+    min-height: 67vh;
+    @media (max-width: 600px) {
+        justify-content: center;
+    }
+`
+
+export const DivInfo = styled.div`
+    display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     background-color: #ffffff;
-    min-height: 67vh;
 `
 
 export const H2About = styled.h2`
@@ -28,5 +38,35 @@ export const PAbout = styled.p`
 `
 
 export const DivText = styled.div`
-    max-width: 62vw;
+    max-width: 30vw;
+    @media (max-width: 800px) {
+        max-width: 35vw;
+    }
+    @media (max-width: 700px) {
+        max-width: 40vw;
+    }
+    @media (max-width: 600px) {
+        max-width: 70vw;
+    }
+`
+
+export const ImgAbout = styled.img`
+    height: 500px;
+    width: auto;
+    border-radius: 20px;
+    @media (max-width: 800px) {
+        height: 450px;
+        width: auto;
+    }
+    @media (max-width: 700px) {
+        height: 400px;
+        width: auto;
+    }
+    @media (max-width: 600px) {
+        display: none
+    }
+`
+
+export const DivImg = styled.div`
+    background-color: #ffffff;
 `
